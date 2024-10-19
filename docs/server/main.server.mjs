bec00296be836ba6842db25c5de4683ffc24ef57
Chamith Mendis <m.chamith@directfn.com>
2024-10-19 12:42:26 +0530

    build test

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-HBK7IBUN.mjs";import"./chunk-WENPLILQ.mjs";import"./chunk-R7F2IGWA.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-56ZR5HX3.mjs";import"./chunk-IWHO7UR5.mjs";import"./chunk-26ONN7HL.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
