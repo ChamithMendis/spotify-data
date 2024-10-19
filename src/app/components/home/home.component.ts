fbc63377945c10d8156ac0a0d6358cf0ed638845
Chamith Mendis <m.chamith@directfn.com>
2024-10-19 08:09:25 +0530

    Populate table from API details

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,43 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { HomeService } from '../../services/home/home.service';
 import { error } from 'console';
-import { MatTableDataSource } from '@angular/material/table';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 interface searchItem {
   value: string;
   viewValue: string;
 }
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
+// export interface SpotifyTrackDetails {
+//   position: number;
+//   trackName: string;
+//   primaryArtist: string;
+//   additionalArtist: string;
+//   albumName: string;
+//   label: string;
+//   isrc: string;
+//   upc: string;
+//   trackLength: string;
+//   lifetimeStreams: string;
+//   streamsInThePastYear: string;
+// }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
+// const ELEMENT_DATA: SpotifyTrackDetails[] = [];
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+  dataArray: Array<any> = new Array();
+
   seachItemList: searchItem[] = [
-    { value: 'artist', viewValue: 'Search by artist' },
-    { value: 'album', viewValue: 'Search by album' },
+    // { value: 'artist', viewValue: 'Search by artist' },
+    // { value: 'album', viewValue: 'Search by album' },
     { value: 'track', viewValue: 'Search by track' },
   ];
 
@@ -46,13 +45,38 @@ export class HomeComponent implements OnInit {
     selectItem: new FormControl(''),
   });
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  displayedColumns: string[] = [
+    'position',
+    'trackName',
+    'primaryArtist',
+    'additionalArtist',
+    'albumName',
+    'label',
+    'isrc',
+    'upc',
+    'trackLength',
+    'lifetimeStreams',
+    'streamsInThepastYear',
+  ];
+  dataSource: MatTableDataSource<any>;
 
-  constructor(private _homeService: HomeService) {}
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private _homeService: HomeService) {
+    this.dataSource = new MatTableDataSource([{}]);
+  }
 
   ngOnInit(): void {
     this.getAccess();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
   public getAccess(): void {
@@ -87,6 +111,10 @@ export class HomeComponent implements OnInit {
       .subscribe(
         (res) => {
           console.log(res);
+          this.populateDataTable(res?.tracks?.items);
+          // set data to ELEMENT_DATA
+          // this.dataArray = [{ upc: '1' }];
+          this.dataSource = new MatTableDataSource(this.dataArray);
         },
         (error) => {
           console.log(error);
@@ -95,8 +123,54 @@ export class HomeComponent implements OnInit {
     // this.searchForm.reset();
   }
 
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+  public clearTable(): void {
+    this.dataArray = [];
+  }
+
+  public populateDataTable(items: any): void {
+    if (items.length <= 0) {
+      return;
+    }
+    this.clearTable();
+
+    items.forEach((element: any) => {
+      const dataObject = {
+        position: 0,
+        trackName: element.name,
+        primaryArtist: element?.artists[0]?.name,
+        additionalArtist: this.getAdditionalArtists(element?.artists),
+        albumName: element.album.name,
+        label: '',
+        isrc: element?.external_ids?.isrc,
+        upc: element?.external_ids?.upc,
+        trackLength: this.msToMinutes(element.duration_ms),
+        lifetimeStreams: '',
+        streamsInThepastYear: '',
+      };
+
+      this.dataArray.push(dataObject);
+    });
+  }
+
+  public msToMinutes(ms: any): any {
+    let seconds = ms / 1000;
+    let minutes = seconds / 60;
+    return minutes;
+  }
+
+  public getAdditionalArtists(artists: any): any {
+    // element?.artists[0]?.name
+    if (artists.length <= 0) {
+      return;
+    }
+
+    let additionalNames = '';
+
+    if (artists[1]) {
+      for (let index = 1; index < artists.length; index++) {
+        additionalNames = additionalNames + artists[index].name + ', ';
+      }
+    }
+    return additionalNames;
   }
 }
