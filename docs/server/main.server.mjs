1bda4dface418ae3e4310dff0c2c5286f194732c
Chamith Mendis <m.chamith@directfn.com>
2024-10-19 13:21:32 +0530

    build changes

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-FAU33F22.mjs";import"./chunk-ROEI7ORU.mjs";import"./chunk-KGZONNAI.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-26BLHAG7.mjs";import"./chunk-ROEI7ORU.mjs";import"./chunk-KGZONNAI.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
